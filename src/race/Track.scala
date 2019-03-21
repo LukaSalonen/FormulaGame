d2c9f68dae6834837eeb7c9de64d251b8c995cbc
Luka Salonen <luka.salonen@aalto.fi>
2019-03-21 15:01:58 +0200

    Modified moveCar method

--- src/race/Track.scala
+++ src/race/Track.scala
@@ -5,7 +5,11 @@ class Track(private val raceTrack: Array[Array[SquareType]], private val cars: A
   //Tuple of coordinates, first is the current location, second is the last one.
   private val carLocations: Array[(Coordinates, Coordinates)] = ???  //TODO
   
-  def moveCar(car: Car, newLocation: Coordinates) = ??? //TODO  
+  //Does not currently check if square can be driven to.
+  def moveCar(car: Car, newLocation: Coordinates) = {
+    val carNumber = this.cars.indexOf(car)
+    this.carLocations(carNumber) = (newLocation, carLocations(carNumber)._1)
+  }
   
   def checkCollision(point1: Coordinates, point2: Coordinates) = ??? //TODO
   
