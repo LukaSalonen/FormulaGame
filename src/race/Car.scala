28c6c00a6c2d8188b350b10b8fdd073557f84dbd
Luka Salonen <luka.salonen@aalto.fi>
2019-03-25 00:08:48 +0200

    Added crashed variable

--- src/race/Car.scala
+++ src/race/Car.scala
@@ -4,6 +4,7 @@ package race
 class Car(val driver: Driver) {
   
   private var turnsTaken = 0
+  var crashed = false
   
   def increaseTurnsTaken = turnsTaken +=1
   def getTurnsTaken = this.turnsTaken
