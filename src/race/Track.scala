d36373332b2772d3d9137532348c1dcd34951a6d
Luka Salonen <luka.salonen@aalto.fi>
2019-03-25 13:32:23 +0200

    Added helper methods for cars and squares

--- src/race/Track.scala
+++ src/race/Track.scala
@@ -1,17 +1,57 @@
 package race
 
 class Track(private val raceTrack: Array[Array[SquareType]], private val cars: Array[Car]) {
-  
-  //Tuple of coordinates, first is the current location, second is the last one.
-  private val carLocations: Array[(Coordinates, Coordinates)] = ???  //TODO
-  
-  //Does not currently check if square can be driven to. TODO
-  def moveCar(car: Car, newLocation: Coordinates) = {
-    val carNumber = this.cars.indexOf(car)
-    this.carLocations(carNumber) = (newLocation, carLocations(carNumber)._1)
+
+  val previousLocation: Array[Coordinates] = Array.ofDim[Coordinates](cars.length)
+
+  placeCarsOnStart
+
+  private def placeCarsOnStart = {
+    var startingSquares = scala.collection.mutable.Buffer[SquareType]()
+
+    for (i <- raceTrack.indices) {
+      for (j <- raceTrack(i).indices) {
+        val current = raceTrack(i)(j)
+        if (current.toString == "StartingPlace") startingSquares += current
+      }
+    }
+
+    if (startingSquares.length < cars.length) println("placingCarsOnStart kusee") //TODO throw exception
+
+    else {
+      startingSquares = scala.util.Random.shuffle(startingSquares)
+
+      for (k <- cars.indices) {
+        startingSquares(k).carHere = Some(cars(k))
+        previousLocation(k) = coordinatesOfSquare(startingSquares(k))
+      }
+
+    }
   }
-  
-  def checkCollision(point1: Coordinates, point2: Coordinates) = ??? //TODO
-  
-  def checkVictory = ??? //TODO
+
+  private def coordinatesOfSquare(square: SquareType): Coordinates = {
+    var result = new Coordinates(-1, -1)
+    for (i <- raceTrack.indices) {
+      for (j <- raceTrack(i).indices) {
+        if (square == raceTrack(i)(j)) result = new Coordinates(j, i)
+      }
+    }
+    result
+  }
+
+  override def toString = {
+    var result = ""
+
+    for (i <- raceTrack.indices) {
+      result += "|"
+      for (j <- raceTrack(i).indices) {
+        val current = raceTrack(i)(j)
+        result += (if (current.toString == "Driveway" && current.carHere == None) "." else if (current.toString == "Obstacle") "b" else if (current.carHere != None) "X" else "s")
+      }
+      result += "|\n"
+    }
+
+    result
+  }
+
 }
