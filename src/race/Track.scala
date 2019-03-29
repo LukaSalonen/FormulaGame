d819ff548ff610073290509e2ce80766cda33fa6
Luka Salonen <luka.salonen@aalto.fi>
2019-03-29 20:40:27 +0200

    added methods for moving cars

--- src/race/Track.scala
+++ src/race/Track.scala
@@ -1,12 +1,87 @@
 package race
 
-class Track(private val raceTrack: Array[Array[SquareType]], private val cars: Array[Car]) {
+class Track(private val raceTrack: Array[Array[SquareType]], val cars: Array[Car]) {
+
+  val width = raceTrack.head.length
+  val height = raceTrack.length
 
   val previousLocation: Array[Coordinates] = Array.ofDim[Coordinates](cars.length)
 
   placeCarsOnStart
 
+  def moveOptions(playerIndex: Int): Array[Coordinates] = {
+
+    var result = Array.ofDim[Coordinates](9)
+    val currentLocation = locationOfCar(playerIndex)
+    val lastLocation = previousLocation(playerIndex)
+    var nextCenter = currentLocation.moveTheDifference(lastLocation)
+
+    if (currentLocation.equals(lastLocation)) {
+      val closestGoal = closestGoalLine(currentLocation).getOrElse(new Coordinates(currentLocation.x + 1, currentLocation.y))
+      if (currentLocation.x != closestGoal.x) {
+        if (currentLocation.x > closestGoal.x) nextCenter = new Coordinates(currentLocation.x + 1, currentLocation.y)
+        else nextCenter = new Coordinates(currentLocation.x - 1, currentLocation.y)
+      } else if (currentLocation.y != closestGoal.y) {
+        if (currentLocation.y > closestGoal.y) nextCenter = new Coordinates(currentLocation.x, currentLocation.y + 1)
+        else nextCenter = new Coordinates(currentLocation.x, currentLocation.y - 1)
+      }
+    }
+
+    result(0) = nextCenter
+    result(1) = nextCenter.addToXY(-1, -1)
+    result(2) = nextCenter.addToXY(0, -1)
+    result(3) = nextCenter.addToXY(1, -1)
+    result(4) = nextCenter.addToXY(-1, 0)
+    result(5) = nextCenter.addToXY(1, 0)
+    result(6) = nextCenter.addToXY(-1, 1)
+    result(7) = nextCenter.addToXY(0, 1)
+    result(8) = nextCenter.addToXY(1, 1)
+
+    result = result.filter(a => squareAtPos(a).canPassThrough) //index out of bounds TODO
+    
+    result
+  }
+
+  def locationOfCar(playerIndex: Int): Coordinates = {
+    var result = new Coordinates(-1, -1)
+    for (i <- 0 until this.height) {
+      for (j <- 0 until this.width) {
+        val current = raceTrack(i)(j)
+        if (current.carHere == Some(cars(playerIndex))) result = coordinatesOfSquare(current)
+      }
+    }
+    result
+  }
+
+  def squareAtPos(pos: Coordinates) = raceTrack(pos.y)(pos.x)
+
+  def moveCar(playerIndex: Int, target: Coordinates) = {
+    val currentPos = locationOfCar(playerIndex)
+    if (!currentPos.equals(target)) {
+      val currentSquare = squareAtPos(currentPos)
+      val targetSquare = squareAtPos(target)
+
+      targetSquare.carHere = currentSquare.carHere
+      currentSquare.carHere = None
+      previousLocation(playerIndex) = currentPos
+    }
+  }
+
+  private def closestGoalLine(pos: Coordinates): Option[Coordinates] = {
+    var result: Option[Coordinates] = None
+    for (i <- 1 to 5) {
+      if (result.isEmpty) {
+        var testArray = Array(new Coordinates(pos.x - i, pos.y), new Coordinates(pos.x + i, pos.y),
+          new Coordinates(pos.x, pos.y - i), new Coordinates(pos.x, pos.y + i))
+
+        result.foreach(currentPos => (if (raceTrack(currentPos.x)(currentPos.y).toString == "GoalLine") result = Some(currentPos)))
+      }
+    }
+    result
+  }
+
   private def placeCarsOnStart = {
+
     var startingSquares = scala.collection.mutable.Buffer[SquareType]()
 
     for (i <- raceTrack.indices) {
@@ -15,7 +90,7 @@ class Track(private val raceTrack: Array[Array[SquareType]], private val cars: A
         if (current.toString == "StartingPlace") startingSquares += current
       }
     }
-
+    require(startingSquares.length >= cars.length)
     if (startingSquares.length < cars.length) println("placingCarsOnStart kusee") //TODO throw exception
 
     else {
