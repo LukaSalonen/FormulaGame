0e6b3dc97adc3edda99bbd08d71d97024ae1d7c3
Luka Salonen <luka.salonen@aalto.fi>
2019-04-09 19:10:10 +0300

    ignored backup files

--- src/race/LapTime.scala
+++
@@ -1,7 +0,0 @@
-package race
-
-class LapTime(val driver: Driver, val nameOfTrack: String, private var turnsTaken: Int = 0) {
-  
-  def incrementTurns = turnsTaken += 1
-  def getTurns = turnsTaken
-}
